9fcd409e0fa12cc1b9baaec4c5b92b98a854f737
Jacobs <boombacks2711@gmail.com>
2023-07-25 18:33:11 +0300

    ADD: Добавлен конфиг, Notifier, MakrdownReplacer

---
+++ internal/config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"log"
+	"sync"
+	"time"
+
+	"github.com/cristalhq/aconfig"
+	"github.com/cristalhq/aconfig/aconfighcl"
+)
+
+type Config struct {
+	TelegramBotToken     string        `hcl:"telegram_bot_token" env:"TELEGRAM_BOT_TOKEN" required:"true"`
+	TelegramChannelID    int64         `hcl:"telegram_channel_id" env:"TELEGRAM_CHANNEL_ID" required:"true"`
+	DatabaseDSN          string        `hcl:"database_dsn" env:"DATABASE_DSN" default:"postgres://postgres:postgres@localhost:5432/news_feed_bot?sslmode=disable"`
+	FetchInterval        time.Duration `hcl:"fetch_interval" env:"FETCH_INTERVAL" default:"10m"`
+	NotificationInterval time.Duration `hcl:"notification_interval" env:"NOTIFICATION_INTERVAL" default:"1m"`
+	FilterKeywords       []string      `hcl:"filter_keywords" env:"FILTER_KEYWORDS"`
+	OpenAIKey            string        `hcl:"openai_key" env:"OPENAI_KEY"`
+	OpenAIPrompt         string        `hcl:"openai_prompt" env:"OPENAI_PROMPT"`
+}
+
+var (
+	cfg  Config
+	once sync.Once //Этот примитив гарантирует, что метод вызванный через него вызывиться ОДИН раз
+	//Это полезно, так как мы будем триггерить метод из разных мест в разном порядке
+)
+
+func Get() Config {
+	once.Do(func() {
+		loader := aconfig.LoaderFor(&cfg, aconfig.Config{
+			EnvPrefix: "NFB", //Префикс конфиг файлов, чтобы не было конфликтов с другими файлами
+			Files:     []string{"./config.hcl", "./config.local.hcl"},
+			FileDecoders: map[string]aconfig.FileDecoder{
+				".hcl": aconfighcl.New(),
+			},
+		})
+
+		if err := loader.Load(); err != nil {
+			log.Printf("[ERROR] failed to load config: %v", err)
+		}
+	})
+
+	return cfg
+}
